c61ec41389c9c5145e03c6d9bde1c18b60e1bc41
MYCOM <MYCOM@DESKTOP-7VSSDBG>
2024-02-02 13:08:36 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'origin' of
    https://github.com/dahee2462/second_team_project.git into origin

--- teamproject_academy/src/main/java/util/DBManager.java
+++ teamproject_academy/src/main/java/util/DBManager.java
@@ -4,103 +4,230 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class DBManager {
 	
-	String url = "jdbc:mysql://localhost:3306/board";
-	String user = "tester";
-	String pass ="1234";
+	// [필드]
+	private String url;
+	private String user;
+	private String pass;
 	
-	Connection conn = null;
-	PreparedStatement psmt = null;
-	Statement stmt = null;
-	ResultSet rs = null;
+	private Connection conn=null;
+	private PreparedStatement psmt = null;
+	private ResultSet rs = null;
 	
-	//1. DB메소드
-	//(1) DB 연결 메소드
-	public boolean DBOpen() {
+	// 내부 카운터 : psmt 숫자++
+	private int orderCount = 1; 
+
+	// [생성자]
+	public DBManager() {
+		this.url= "jdbc:mysql://localhost:3306/board";
+		this.user = "tester";
+		this.pass = "1234";
+	}
+	
+	// [게터세터]
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
+	public Connection getConn() {
+		return conn;
+	}
+
+	// [메소드]
+	//DB 연결
+	public boolean connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(url, user, pass);
+			this.conn = DriverManager.getConnection(url, user, pass);
 		}catch(Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	//(2) DB 종료 메소드
-	public boolean DBClose() {
+	//DB 종료
+	public boolean disconnect() {
 		try {
-			this.conn.close();
-			return true;	
+			//쿼리닫음
+			release();
+			if(conn != null) conn.close();
+				
 		}catch(Exception e) {
+			e.printStackTrace();
 			return false;
 		}
+		return true;
 	}
 	
-	//2. SQL메소드
-	//(1) SQL 실행 메소드
-	// - insert, update, delete
-	public boolean RunSql(String sql) {
+	//psmt
+	public DBManager prepare(String sql) {
+		
+		if(conn == null) return null;
+		
 		try {
+			//쿼리닫음
+			release(); // rs, state 모두 초기화.?
 			
-			this.stmt = this.conn.createStatement();
-			this.psmt.executeUpdate(sql);
-			
-			this.psmt.close();
-			return true;
-		}catch(Exception e) {
-			return false;
-		}
+			psmt = conn.prepareStatement(sql);
 
+			orderCount = 1;
+
+		}catch (Exception e) {
+
+			e.printStackTrace();
+			return null;
+		}
+		
+		return this;
 	}
-	// - select.
-	public boolean OpenQuery(String sql) {
+	
+	//파라미터 비우기
+	public DBManager clearParameter() {
+		
+		orderCount=1;
+		
+		if(psmt!=null){
+			try {
+				psmt.clearParameters();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return this;
+	}
+	
+	// set 파라미터
+
+	public DBManager setInt(int val) {
+		if(psmt != null) {
+			try {
+				psmt.setInt(orderCount, val);
+			}catch(Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+			
+			orderCount++;
+			return this;
+		}
+		
+		return null;
+	}
+	
+	public DBManager setString(String s) {
+		if(psmt != null) {
+			try {
+				psmt.setString(orderCount, s);
+			}catch(Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+			
+			orderCount++;
+			return this;
+		}
+		
+		return null;
+	}
+	
+
+	//update,insert,delete -> 트랜잭션 필요
+	public int update() throws SQLException{
+		try {
+			//트랜잭션 시작
+			conn.setAutoCommit(false);
+			if(psmt!=null) {
+				return psmt.executeUpdate();
+			}
+			//커밋
+			conn.commit();
+		}catch(Exception e) {
+			//롤백
+			conn.rollback();
+			e.printStackTrace();
+		}finally {
+			//트랜잭션 원복
+			conn.setAutoCommit(true);
+		}
+		return 0;
+	}
+	
+	//select
+	public boolean select(){
+		try {
+			if(psmt!=null) {
+				if(rs != null) {
+					rs.close();
+				}
+				rs = psmt.executeQuery();
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	//close query
+	public boolean release(){
 		try {
 			
-			this.stmt=this.conn.createStatement();
-			this.rs=this.psmt.executeQuery(sql);
-			return true;
+			if(rs != null) rs.close();
+			if(psmt != null) psmt.close();
+			
 		}catch(Exception e) {
+			
 			return false;
 		}
+		return true;
 	}
-	//(2) SQL 종료 메소드
-	public boolean CloseQuery() {
-		try {
-			if (conn != null) conn.close();
-			if (psmt != null) psmt.close();
-			if (rs != null) rs.close();
-			return true;
-		}catch(Exception e) {
+	
+	
+	// rs의 next() 실행할 메소드
+	public boolean getNext() {
+		if(rs == null)
 			return false;
-		}
-	}
-	// - result next()를 실행할 메소드
-	public boolean GetNext() {
-		try {
+		
+		try {	
 			return this.rs.next();
 		}catch(Exception e) {
 			return false;
 		}
 	}
-	// -- 반환값 String일 때
-	public String GetValue(String colName) {
+	
+	//-get 파라미터
+	// result로부터 값을 받아올 메소드: int, String 등등
+	public String getString(String colName ) {
 		try {
-			String str = this.rs.getString(colName);
-			return str;
+			return this.rs.getString(colName);
 		}catch(Exception e) {
+			
 			return null;
 		}
 	}
-	// -- 반환값 int일 때
-	public int GetInt(String colName) {
+	public int getInt(String colName ) {
 		try {
 			return this.rs.getInt(colName);
 		}catch(Exception e) {
 			return 0;
 		}
 	}
+	
+	// 예외처리 메소드 - '를 처리하는 메소드
+	protected String _R(String value) {
+		//return value.replace("'","''").replace(" ",""));
+		return value.replace("'","''");
+	}
+	
 
 }
