bfa89e2266ed1201cd7c178f43ed78040bff59c8
MYCOM <MYCOM@DESKTOP-DK8REN4>
2024-02-16 15:17:04 +0900

    로그인 완료시 URL 포워딩 확인, 학생,교수,관리자별 이동 확인

--- teamproject_academy/src/main/java/common/controller/IndexController.java
+++ teamproject_academy/src/main/java/common/controller/IndexController.java
@@ -93,23 +93,22 @@ public class IndexController {
 			
 		}
 		if(isValidPassword) {
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/common/index.jsp");
 			request.getSession().setAttribute("type",typeData);
 			request.getSession().setAttribute("id",idParam);
-			if(typeData.equals("student")) {
-				rd = request.getRequestDispatcher("/WEB-INF/student/stuMain.jsp");
-			}else if(typeData.equals("professor")) {
-				rd = request.getRequestDispatcher("/WEB-INF/professor/profMain.jsp");
-			}else if(typeData.equals("administer")) {
-				rd = request.getRequestDispatcher("/WEB-INF/admin/admMain.jsp");
-			}
 			//아이디 기억 쿠키
 			if(rememberParam != null && rememberParam.equals("check")) {
 				Cookie cookie = new Cookie("rememberParam",idParam);
 				cookie.setMaxAge(60*60*3);
 				response.addCookie(cookie);
 			}
-			rd.forward(request, response);
+			if(typeData.equals("student")) {
+				response.sendRedirect(request.getContextPath()+"/student/stuMain");
+			}else if(typeData.equals("professor")) {
+				response.sendRedirect(request.getContextPath()+"/professor/profMain");
+			}else if(typeData.equals("administer")) {
+				response.sendRedirect(request.getContextPath()+"/admin/admMain");
+			}
+			
 		}else {
 			response.setContentType("text/html; charset=utf-8");
 			response.setCharacterEncoding("UTF-8");
